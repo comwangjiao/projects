fecb66174b91501b05208a4472fbbc7a08c9e5fa
v_emersonwang <v_emersonwang@lexinfintech.com>
2018-08-15 17:02:29 +0800

    第一个git

---
+++ com-test/src/main/java/RunGit.java
@@ -0,0 +1,10 @@
+/**
+ * @Author:emerson
+ * @Description:
+ * @Date: 2018/8/15 16:58
+ */
+public class RunGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
